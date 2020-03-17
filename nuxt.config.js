3e1fb079745479568b7a19fc683f9a84a03671b9
ww <wwjobs@163.com>
2020-03-17 11:39:51 +0800

    修改页面tittle

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || 'W 的博客',
+    title: 'W 的博客',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    // link: [
+    //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    // ]
   },
   /*
   ** Customize the progress-bar color
