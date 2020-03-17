5b678b94066f847b0869e18adcb0cc26aba2ea06
ww <wwjobs@163.com>
2020-03-17 18:25:17 +0800

    移动端输入，禁止放大

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
     title: 'W 的博客',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     // link: [
