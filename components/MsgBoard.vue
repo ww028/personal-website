ac9b69d9489e3e693d2edabd250bebd0ad2f7243
ww <wwjobs@163.com>
2020-04-13 17:56:02 +0800

    修改留言板

--- components/MsgBoard.vue
+++ components/MsgBoard.vue
@@ -73,6 +73,7 @@ export default {
       ).then(res =>{
         this.msg_list = this.msg_list.concat(res.data)
         // this.msg_list.(res.data)
+        console.log(res)
         this.total = res.total
         this.total_page = res.total_page
       })
