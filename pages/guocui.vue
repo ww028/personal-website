7157f17955dd23d1489933fb7045019620355f87
ww <wwjobs@163.com>
2020-06-08 11:01:45 +0800

    save

--- pages/guocui.vue
+++ pages/guocui.vue
@@ -99,10 +99,10 @@ export default {
   },
 
   mounted() {
-    console.log('mounted')
-    api.GameList().then(res =>{
-      console.log(res)
-    })
+    // console.log('mounted')
+    // api.GameList().then(res =>{
+    //   console.log(res)
+    // })
     this.chartsInit();
     // api.dataAnalysisEdit({
     //   w_ip: returnCitySN['cip'],
@@ -112,75 +112,75 @@ export default {
   },
 
   methods: {
-    personalChartInit() {
-      console.log(this.tableDataMembers)
-      // 找到容器
-      let personal_chart = this.$echarts.init(
-        document.getElementById("personal_chart")
-      );
-      personal_chart.setOption({
-        title: {
-          text: "折线图堆叠"
-        },
-        tooltip: {
-          trigger: "axis"
-        },
-        legend: {
-          data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "3%",
-          containLabel: true
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
-        },
-        xAxis: {
-          type: "category",
-          boundaryGap: false,
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-        },
-        yAxis: {
-          type: "value"
-        },
-        series: [
-          {
-            name: "邮件营销",
-            type: "line",
-            stack: "总量",
-            data: [120, 132, 101, 134, 90, 230, 210]
-          },
-          {
-            name: "联盟广告",
-            type: "line",
-            stack: "总量",
-            data: [220, 182, 191, 234, 290, 330, 310]
-          },
-          {
-            name: "视频广告",
-            type: "line",
-            stack: "总量",
-            data: [150, 232, 201, 154, 190, 330, 410]
-          },
-          {
-            name: "直接访问",
-            type: "line",
-            stack: "总量",
-            data: [320, 332, 301, 334, 390, 330, 320]
-          },
-          {
-            name: "搜索引擎",
-            type: "line",
-            stack: "总量",
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
-          }
-        ]
-      });
-    },
+    // personalChartInit() {
+    //   console.log(this.tableDataMembers)
+    //   // 找到容器
+    //   let personal_chart = this.$echarts.init(
+    //     document.getElementById("personal_chart")
+    //   );
+    //   personal_chart.setOption({
+    //     title: {
+    //       text: "折线图堆叠"
+    //     },
+    //     tooltip: {
+    //       trigger: "axis"
+    //     },
+    //     legend: {
+    //       data: ["邮件营销", "联盟广告", "视频广告", "直接访问", "搜索引擎"]
+    //     },
+    //     grid: {
+    //       left: "3%",
+    //       right: "4%",
+    //       bottom: "3%",
+    //       containLabel: true
+    //     },
+    //     toolbox: {
+    //       feature: {
+    //         saveAsImage: {}
+    //       }
+    //     },
+    //     xAxis: {
+    //       type: "category",
+    //       boundaryGap: false,
+    //       data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
+    //     },
+    //     yAxis: {
+    //       type: "value"
+    //     },
+    //     series: [
+    //       {
+    //         name: "邮件营销",
+    //         type: "line",
+    //         stack: "总量",
+    //         data: [120, 132, 101, 134, 90, 230, 210]
+    //       },
+    //       {
+    //         name: "联盟广告",
+    //         type: "line",
+    //         stack: "总量",
+    //         data: [220, 182, 191, 234, 290, 330, 310]
+    //       },
+    //       {
+    //         name: "视频广告",
+    //         type: "line",
+    //         stack: "总量",
+    //         data: [150, 232, 201, 154, 190, 330, 410]
+    //       },
+    //       {
+    //         name: "直接访问",
+    //         type: "line",
+    //         stack: "总量",
+    //         data: [320, 332, 301, 334, 390, 330, 320]
+    //       },
+    //       {
+    //         name: "搜索引擎",
+    //         type: "line",
+    //         stack: "总量",
+    //         data: [820, 932, 901, 934, 1290, 1330, 1320]
+    //       }
+    //     ]
+    //   });
+    // },
 
     chartsInit() {
       // 找到容器
